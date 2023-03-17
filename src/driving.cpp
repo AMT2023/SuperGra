b07f710fae41a4e2cbead45937a0e44df00c04e3
SzaasSnoo <zjadlem4wengorze@gmail.com>
2023-03-17 18:37:41 +0100

    Jeżdżenie

--- src/driving.cpp
+++ src/driving.cpp
@@ -1,4 +1,58 @@
 #include <SFML/Graphics.hpp>
-int driving(auto & window ){
-    
+#include <cmath>
+
+void driving(float &angle, float &speed, sf::RectangleShape &rect)
+{
+    speed /= 4;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+    {
+        angle -= 2;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+    {
+        angle += 2;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+    {
+        speed += 2;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+    {
+        speed -= 2;
+    }
+    rect.setRotation(angle);
 }
+
+class Player
+{
+    public:
+    sf::RectangleShape rect;
+    float angle;
+    float speed;
+
+    Player(sf::Vector2f position)
+    {
+        rect.setPosition(position);
+        rect.setSize({40, 40});
+        rect.setOrigin(rect.getSize().x / 2, rect.getSize().y / 2);
+    }
+
+    void move()
+    {
+        rect.move({
+            speed * sin(M_PI / 180 * angle),
+            -speed * cos(M_PI / 180 * angle)
+        });
+    }
+
+    void blit(sf::RenderWindow &window)
+    {
+        window.draw(rect);
+    }
+
+    void update(sf::RenderWindow &window)
+    {
+        move();
+        blit(window);
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,28 @@
 #include <SFML/Graphics.hpp>
 #include "driving.cpp"
+
 int main()
 {
     auto window = sf::RenderWindow{ { 1920u, 1080u }, "Passaratti Raceing" };
-    //nie dajemy limitu FPS
+    window.setFramerateLimit(144);
 
-    auto kwadrat = sf::RectangleShape({100, 100});
+    Player player({100, 110});
 
     while (window.isOpen())
     {
-
-
         for (auto event = sf::Event{}; window.pollEvent(event);)
         {
             if (event.type == sf::Event::Closed)
             {
                 window.close();
-
-                
             }
         }
 
         window.clear();
+
+        driving(player.angle, player.speed, player.rect);
+        player.update(window);
+
         window.display();
     }
 }
