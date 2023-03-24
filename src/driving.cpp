2b1b7383a1ec7c50235f2cd8b9444700d2bd2bbc
kachim2 <michalrog14@gmail.com>
2023-03-24 17:38:14 +0100

    FIXED A LOT of stuff

--- src/driving.cpp
+++ src/driving.cpp
@@ -1,45 +1,6 @@
-#include <SFML/Graphics.hpp>
 #include <cmath>
 #include "particles.cpp"
-
-class Player
-{
-    public:
-    sf::RectangleShape rect;
-    sf::Texture tex;
-    float angle = 0;
-    float speed;
-
-    float PRZYSPIESZENIE = 0.15;
-
-    Player(sf::Vector2f position)
-    {
-        rect.setPosition(position);
-        rect.setSize({40, 60}); // rect.setSize({63, 128});
-        rect.setOrigin(rect.getSize().x / 2, rect.getSize().y / 2);
-
-        // tex.loadFromFile("passaratti-no-bg.png");
-        // rect.setTexture(&tex);
-    }
-
-    void move()
-    {
-        speed *= 0.98;
-        rect.move({
-            speed * (float)(sin(M_PI / 180 * angle)),
-            speed * (float)(-cos(M_PI / 180 * angle))
-        });
-    }
-
-    void update(sf::RenderWindow &window)
-    {
-        move();
-        // auto view = sf::View(rect.getPosition(), { 1920u, 1080u });
-        // view.setRotation(angle);
-        // window.setView(view);
-        window.draw(rect);
-    }
-};
+#include"player.cpp"
 
 void driving(Player &player)
 {
--- src/shop.cpp
+++ src/shop.cpp
@@ -1,5 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include"player.cpp"
 #include <cmath>
 #include <random>
-#include "particles.cpp"
 
